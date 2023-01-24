798a572d9363a32e5e9d3ac9b35808814f52b378
berkaygp <19290307@ogrenci.ankara.edu.tr>
2023-01-24 17:18:13 +0300

    outputting conditional task content

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,15 +3,20 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = (props) => {
+
+  let tasksContent = <p>No tasks found!</p>;
+  if (props.ourTasks.length > 0) {
+    tasksContent = props.ourTasks.map((task) => (
+      <Task 
+        key={task.id}
+        title={task.title}
+      />
+    ))
+  }
   
     return (
         <div className="task-list">
-          {props.ourTasks.map((task) => (
-            <Task 
-              key={task.id}
-              title={task.title}
-            />
-          ))}
+          {tasksContent}
         </div>
       
     );
