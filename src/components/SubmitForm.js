b9541c5d3029f09076894111e379efe3cbfb4c07
berkaygp <19290307@ogrenci.ankara.edu.tr>
2023-01-26 00:49:18 +0300

    prevent adding empty task

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 
 const SubmitForm = (props) => {
     const [enteredTask, setEnteredTask] = useState('');
-
+    const [isValid, setIsValid] = useState(true);
 
     const changeHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
         setEnteredTask(event.target.value);
     };
 
@@ -15,14 +18,23 @@ const SubmitForm = (props) => {
             title: enteredTask
         };
 
+        if (enteredTask.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
         props.onSaveTaskData(taskData);
         setEnteredTask('');
     };
 
     return (
         <form onSubmit={submitHandler}>
-            <input type="search" placeholder="write sth" value={enteredTask} onChange={changeHandler}/>
-            <input type="submit" value="add"/>
+            <input
+                style={{ borderColor: !isValid ? 'red' : 'black' }}
+                type="text" placeholder="write sth"
+                value={enteredTask}
+                onChange={changeHandler}
+            />
+            <input type="submit" value="add" />
         </form>
     );
 }
