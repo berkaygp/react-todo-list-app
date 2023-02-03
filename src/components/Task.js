daa91e2c9ffb32fed6fd1e8c675b3f52b5377c67
berkaygp <19290307@ogrenci.ankara.edu.tr>
2023-02-03 19:14:56 +0300

    complete task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TaskDate from './TaskDate';
 import './Task.css';
 
 const Task = (props) => {
+    const [isComplete, setIsComplete] = useState(false);
+
     const deleteHandler = () => {
         props.onDelete(props.task.id);
     }
 
+    const completeButtonHandler = () => {
+        setIsComplete(!isComplete);
+    };
+
     return (
-        <div className='task'>
-            <TaskDate date={props.date} />
-            <div className='left'>               
-                <h3>{props.title}</h3>
+        <div className="task">
+            <TaskDate date={props.date} />               
+            <h3 
+                style={{
+                    textDecoration: isComplete ? 'line-through' : 'none', 
+                    textDecorationColor: isComplete ? 'red': '',
+                    textDecorationThickness: isComplete ? '0.3rem' : ''
+                }}
+            >{props.title}</h3>
+            <div className="task__buttons">
+                <button className="task__complete-button" onClick={completeButtonHandler}>&#x2713;</button>
+                <button className="task__delete-button" onClick={deleteHandler} >&#x2715;</button>
             </div>
-            <button className='task__delete-button' onClick={deleteHandler} >Delete</button>
         </div>
     );
 }
